01f31baa8c3494527e42adf435e3cdf71afb2b71
meh <meh.ffff@gmail.com>
2009-10-05 15:31:18 +0200

    Added the licence file, AGPLv3 choosed.

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 STAGE1=`stat -c %s test/stage1`
 STAGE2=`stat -c %s test/stage2`
 KERNEL=`stat -c %s lulzKey`
@@ -9,7 +9,7 @@ FLOPPY=`mktemp`
 dd if=/dev/zero of="$TMP" bs=1 count=750 &> /dev/null
 cat test/stage1 test/stage2 "$TMP" lulzKey > "$FLOPPY"
 
-# Uncomment these if the emulator whines about it's not floppy sized.
+# Uncomment these if the emulator whines about it doesn't have enough space.
 # dd if=/dev/zero of="$TMP" bs=1 count=$((1474560-$STAGE1-$STAGE2-$KERNEL-750)) &> /dev/null
 # cat "$TMP" >> "$FLOPPY"
 
