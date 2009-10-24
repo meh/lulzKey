43989a7871277047cfa2685d2b311f0257b47db5
meh <meh.ffff@gmail.com>
2009-10-24 03:14:28 +0000

    Added other life functions dirs.

---
+++ life/sources/System/System.cpp
@@ -0,0 +1,8 @@
+/****************************************************************************
+* Copyright (C) 2009 meh. [http://meh.doesntexist.org]                      *
+*                                                                           *
+* This file is part of lulzKey { life }.                                    *
+*                                                                           *
+* See COPYING or http://www.gnu.org/licenses/agpl-3.0.txt                   *
+****************************************************************************/
+
